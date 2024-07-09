e4e6474e823b19add4a273c4337f07855954af50
Kuo-Chung Lin <Kuo-Chung_Lin@emcore.com>
2024-07-09 11:33:50 -0700

    fix: wrong code file in py test script

--- test.py
+++ test.py
@@ -1,37 +1,37 @@
 import os, subprocess
 
 # Settings
-TEST_DIR = "."  # directory with our program
-CODE_FILE = "main.c." # our C code
-COMPILER_TIMEOUT = 10.0
-RUN_TIMEOUT = 10.0
+TEST_DIR = "."              # Directory with our program
+CODE_FILE = "main.c"        # Our C code
+COMPILER_TIMEOUT = 10.0     # Compiler timeout (seconds)
+RUN_TIMEOUT = 10.0          # Run timeout (seconds)
 
-# create absolute path
+# Create absolute paths
 code_path = os.path.join(TEST_DIR, CODE_FILE)
 app_path = os.path.join(TEST_DIR, "app")
 
 # Compile the program
 print("Building...")
 try:
-    ret = subprocess.run(["gcc", code_path, "-o", app_path],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            timeout=COMPILER_TIMEOUT)
+        ret = subprocess.run(["gcc", code_path, "-o", app_path],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                timeout=COMPILER_TIMEOUT)
 except Exception as e:
     print("ERROR: Compilation failed.", str(e))
     exit(1)
-
+    
 # Parse output
 output = ret.stdout.decode('utf-8')
 print(output)
 output = ret.stderr.decode('utf-8')
 print(output)
 
-# check to see if the program compiled successfully
+# Check to see if the program compiled successfully
 if ret.returncode != 0:
-    print("Compilation failed")
+    print("Compilation failed.")
     exit(1)
-
+    
 # Run the compiled program
 print("Running...")
 try:
@@ -41,15 +41,11 @@ try:
 except Exception as e:
     print("ERROR: Runtime failed.", str(e))
     exit(1)
-
+    
 # Parse output
 output = ret.stdout.decode('utf-8')
 print("Output:", output)
 
-# ALL tests passed, exit gracefully
-print("All tests passed")
+# All tests passed! Exit gracefully
+print("All tests passed!")
 exit(0)
-
-
-
-
